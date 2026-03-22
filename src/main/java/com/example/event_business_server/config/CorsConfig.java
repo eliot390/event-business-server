e8eb40ffa042365eaac146a80ac589cb238977aa
eliot390 <eliot390@github.com>
2026-03-22 11:04:38 -0700

    modified app.prop and added corsconfig for online deployment

---
+++ src/main/java/com/example/event_business_server/config/CorsConfig.java
@@ -0,0 +1,26 @@
+package com.example.event_business_server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig {
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "https://www.flourandflask.com",
+                        "http://localhost:5173"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+      }
+    };
+  }
+}
